a9525da907d2d973c7f01e7e198a7be4867bc088
Lee Hye Won <thankyou@sookmyung.ac.kr>
2025-12-14 21:10:19 +0900

    fix: fix css issues

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+  assetPrefix: isProd ? "https://ehye1.github.io/github-page/" : "",
 };
 
 export default nextConfig;
